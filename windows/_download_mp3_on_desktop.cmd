7f9d4286f502bee76d4916e4d869fbbec8d35409
Vitaly <sultanbaev.vitaly@gmail.com>
2018-10-05 12:24:10 +0500

    windows version

--- windows/_download_mp3_on_desktop.cmd
+++ windows/_download_mp3_on_desktop.cmd
@@ -2,7 +2,9 @@
 
 set /P urltovideo="URL: "
 
-youtube-dl.exe -x --audio-format "mp3" --audio-quality 0 "%urltovideo%"
+%~dp0\bin\youtube-dl.exe -x --audio-format "mp3" --audio-quality 0 "%urltovideo%"
+
+TIMEOUT /T 3
 
 move *.mp3 "%userprofile%\Desktop\"
 del *.mp3
